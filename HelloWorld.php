618d0d0502647e1b876aa97169a50c121350b730
Ninahf <ninahf1503@gmail.com>
2021-09-27 09:53:21 +0800

    menambahkan tanggal pada file HelloWorld.php

--- HelloWorld.php
+++ HelloWorld.php
@@ -7,7 +7,8 @@
 <?php
 echo "Nina Hafizah"."<br>";
 echo "1910817220010"."<br>";
-echo "Hello World, UTS RPL 2021";
+echo "Hello World, UTS RPL 2021"."<br>";
+echo "Senin, 27 September 2021";
 ?>
 
 </body>
